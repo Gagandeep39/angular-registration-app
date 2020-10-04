829d9da872c56b2eaf4dbfbb20d623d15e71951d
Gagandeep Singh <singh.gagandeep3911@gmail.com>
2020-10-04 22:14:26 +0530

    Created landing, singup, signed-up scaffold

---
+++ src/app/sign-up-completed/sign-up-completed.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { SignUpCompletedComponent } from './sign-up-completed.component';
+
+describe('SignUpCompletedComponent', () => {
+  let component: SignUpCompletedComponent;
+  let fixture: ComponentFixture<SignUpCompletedComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ SignUpCompletedComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(SignUpCompletedComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
