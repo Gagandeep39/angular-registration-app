a9c88cdc75289f488c35c62f486635cac80595c2
Gagandeep Singh <singh.gagandeep3911@gmail.com>
2020-10-05 01:26:28 +0530

    Created User model

---
+++ src/app/models/user.model.ts
@@ -0,0 +1,12 @@
+/**
+ * @author Gagandeep Singh
+ * @email singh.gagandeep3911@gmail.com
+ * @create date 2020-10-05 01:26:14
+ * @modify date 2020-10-05 01:26:14
+ * @desc User model
+ */
+export interface User {
+  name: string;
+  email: string;
+  password: string;
+}
