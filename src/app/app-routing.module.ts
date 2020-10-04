da30ec85f223158a3df4aba4b6dcebe53cd80f24
Gagandeep Singh <singh.gagandeep3911@gmail.com>
2020-10-04 22:14:43 +0530

    Created routes to components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { LandingComponent } from './landing/landing.component';
+import { SignUpCompletedComponent } from './sign-up-completed/sign-up-completed.component';
+import { SignUpComponent } from './sign-up/sign-up.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: LandingComponent, pathMatch: 'full' },
+  { path: 'signUp', component: SignUpComponent, pathMatch: 'full' },
+  { path: 'signedUp', component: SignUpCompletedComponent, pathMatch: 'full'  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LandingComponent } from './landing/landing.component';
+import { SignUpComponent } from './sign-up/sign-up.component';
+import { SignUpCompletedComponent } from './sign-up-completed/sign-up-completed.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LandingComponent,
+    SignUpComponent,
+    SignUpCompletedComponent
   ],
   imports: [
     BrowserModule,
